cc133a01e8b663976c4877da76e0730e11f95bfd
M0hamedAshraf19 <MohamedAshrafBedir@proton.me>
2026-08-27 05:48:03 +0300

    Updated The Equations Test, Fixed Bugs in it and Updated The CSS Files

--- Tests/Math/Equations/Script.js
+++ Tests/Math/Equations/Script.js
@@ -37,15 +37,19 @@ if (getCookie('questions') === null) {
     setCookie('questions', 0)
     setCookie('correct', 0)
 } else if (getCookie('answer') !== null) {
-    alert(getCookie('answer'))
+    let q = JSON.parse(getCookie('question'))
+    if (q[1] == '×') {
+        q[1] = '*'
+    }
+    q = q.join('')
     setCookie('questions', parseInt(getCookie('questions'))+1)
-    if (getCookie('answer') == eval(getCookie('question').replace('×', '*')))
+    if (getCookie('answer') == eval(q))
     {
         setCookie('correct', parseInt(getCookie('correct'))+1)
     }
     else {
         alert(`غلط!
-الاجابة هى: \u202A${getCookie('question')} = ${eval(getCookie('question').replace('×', '*'))}\u202C`)
+الاجابة هى: \u202A${q} = ${eval(q)}\u202C`)
     }
     if (getCookie('question').indexOf('×') >= 0) {
         let multiplyNum0=parseInt(getCookie('multiplyNum0'))
@@ -54,10 +58,10 @@ if (getCookie('questions') === null) {
             multiplyNum1+=1
         } else {
             multiplyNum1=0
-            if (multiplyNum0 < parseInt(getCookie('multiplyEnd'))) {
+            if (multiplyNum0 < getCookie('multiplyEnd')) {
                 multiplyNum0 += 1
             } else {
-                multiplyNum0=parseInt(getCookie('multiplyBegin'))
+                multiplyNum0=getCookie('multiplyBegin')
             }
         }
         setCookie('multiplyNum0', multiplyNum0)
@@ -70,83 +74,97 @@ if (getCookie('questions') === null) {
 document.querySelector('#score').innerHTML=`
     Questions: ${getCookie('questions')}<br>Correct: ${getCookie('correct')}
 `
-let el=''
-
-if (getCookie('OPs') === null) {
-    el=document.querySelector('#chooseOP')
-    el.style.display='block'
-    el.querySelector('form').addEventListener('submit', function(e) {
-        e.preventDefault()
-        let checked=el.querySelectorAll("input[type='checkbox']:checked")
-        let selectedOPs=[]
-        checked.forEach(function(checkbox) {
-            selectedOPs.push(checkbox.value)
-        })
-        if (selectedOPs.length === 0) {
-            alert('عليك أن تختار')
-            return
-        }
-        setCookie('OPs', JSON.stringify(selectedOPs))
-        location.reload()
-    })
-} else {
-    if (JSON.parse(getCookie('OPs')).includes('×') && (getCookie('multiplyBegin') === null || getCookie('multiplyEnd') === null)) {
-        el=document.querySelector('#setMultiplication')
-        el.style.display='block'
-        el.querySelector('form').addEventListener('submit', function(e) {
-            e.preventDefault()
-            let multiplyBegin=el.querySelector("select[name='multiplyBegin']").value
-            let multiplyEnd=el.querySelector("select[name='multiplyEnd']").value
-            setCookie('multiplyBegin', multiplyBegin)
-            setCookie('multiplyEnd', multiplyEnd)
-            setCookie('multiplyNum0', multiplyBegin)
-            setCookie('multiplyNum1', 0)
-            location.reload()
-        })
-    } else {
-        el=document.querySelector('#questionForm')
-        el.style.display='block'
-        let OP=''
-        let num0=[]
-        let num1=[]
-        if (getCookie('question') === null) {
-            OP=JSON.parse(getCookie('OPs'))[Math.floor(Math.random() * JSON.parse(getCookie('OPs')).length)]
-            if (OP === '+') {
-                let l = Math.floor(Math.random() * 3) + 1
-                num0 = Array.from({ length: l }, (_, i) => l > 1 && i === 0 ? Math.floor(Math.random()*8)+1 : Math.floor(Math.random()*10));
-                num0.forEach((n, i) => l > 1 && i === 0 ? num1.push(Math.floor(Math.random()*(9-n))+1) : num1.push(Math.floor(Math.random()*(10-n))));
-                num0 = parseInt(num0.join(""), 10)
-                num1 = parseInt(num1.join(""), 10)
-            } else if (OP === '-') {
-                let l = Math.floor(Math.random() * 3) + 1
-                num0 = Array.from({ length: l }, (_, i) => l > 1 && i === 0 ? Math.floor(Math.random()*9)+1 : Math.floor(Math.random()*10));
-                num0.forEach((n, i) => l > 1 && i === 0 ? num1.push(Math.floor(Math.random()*(n-1))+1) : num1.push(Math.floor(Math.random()*(n+1))));
-                num0 = parseInt(num0.join(""), 10)
-                num1 = parseInt(num1.join(""), 10)
-            } else {
-                num0=parseInt(getCookie('multiplyNum0'))
-                num1=parseInt(getCookie('multiplyNum1'))
-            }
-            setCookie('question', `${num0}${OP}${num1}`)
-        }
-        el.querySelector('#question').textContent=`${getCookie('question')}`
-        el.querySelector('form').addEventListener('submit', function(e) {
-            e.preventDefault()
-            let answer=(el.querySelector("input[type='text']").value).trim()
-            if (answer) {
-                setCookie('answer', answer)
-                location.reload()
-            } else {
-                alert('عليك أن تكتب')
-            }
-        })
-    }
-}
-
 document.querySelector('#score').style.display='block'
 document.querySelector('#buttons').style.display='block'
 
-document.getElementById('answer').focus();
+let el=''
+
+if (getCookie('display') === null) {
+    el=document.querySelector('#chooseDisplay')
+    el.style.display='block'
+    el.querySelector('form').addEventListener('submit', function(e) {
+        e.preventDefault()
+        let display=el.querySelector("select[name=display]").value
+        setCookie('display', display)
+        location.reload()
+    })
+} else{
+    if (getCookie('OPs') === null) {
+        el=document.querySelector('#chooseOP')
+        el.style.display='block'
+        el.querySelector('form').addEventListener('submit', function(e) {
+            e.preventDefault()
+            let checked=el.querySelectorAll("input[type='checkbox']:checked")
+            let selectedOPs=[]
+            checked.forEach(function(checkbox) {
+                selectedOPs.push(checkbox.value)
+            })
+            if (selectedOPs.length === 0) {
+                alert('عليك أن تختار')
+                return
+            }
+            setCookie('OPs', JSON.stringify(selectedOPs))
+            location.reload()
+        })
+    } else {
+        if (JSON.parse(getCookie('OPs')).includes('×') && getCookie('multiplyBegin') === null) {
+            el=document.querySelector('#setMultiplication')
+            el.style.display='block'
+            el.querySelector('form').addEventListener('submit', function(e) {
+                e.preventDefault()
+                let multiplyBegin=el.querySelector("select[name=multiplyBegin]").value
+                setCookie('multiplyBegin', multiplyBegin)
+                setCookie('multiplyEnd', el.querySelector("select[name=multiplyEnd]").value)
+                setCookie('multiplyNum0', multiplyBegin)
+                setCookie('multiplyNum1', '0')
+                location.reload()
+            })
+        } else {
+            el=document.querySelector('#questionForm')
+            let OP=''
+            let num0=[]
+            let num1=[]
+            if (getCookie('question') === null) {
+                OP=JSON.parse(getCookie('OPs'))[Math.floor(Math.random() * JSON.parse(getCookie('OPs')).length)]
+                if (OP === '+') {
+                    let l = Math.floor(Math.random() * 3) + 1
+                    num0 = Array.from({ length: l }, (_, i) => l > 1 && i === 0 ? Math.floor(Math.random()*8)+1 : Math.floor(Math.random()*10));
+                    num0.forEach((n, i) => l > 1 && i === 0 ? num1.push(Math.floor(Math.random()*(9-n))+1) : num1.push(Math.floor(Math.random()*(10-n))));
+                    num0 = num0.join('')
+                    num1 = num1.join('')
+                } else if (OP === '-') {
+                    let l = Math.floor(Math.random() * 3) + 1
+                    num0 = Array.from({ length: l }, (_, i) => l > 1 && i === 0 ? Math.floor(Math.random()*8)+2 : Math.floor(Math.random()*10));
+                    num0.forEach((n, i) => l > 1 && i === 0 ? num1.push(Math.floor(Math.random()*(n-1))+1) : num1.push(Math.floor(Math.random()*(n+1))));
+                    num0 = num0.join('')
+                    num1 = num1.join('')
+                } else {
+                    num0=getCookie('multiplyNum0')
+                    num1=getCookie('multiplyNum1')
+                }
+                setCookie('question', JSON.stringify([num0, OP, num1]))
+            }
+            const q = JSON.parse(getCookie('question'))
+            if (getCookie('display') == 'H') {
+                el.querySelector('#question').innerHTML=`${q[0]+q[1]+q[2]}`
+            } else {
+                el.querySelector('#question').innerHTML=`${q[0]+'<br>'+'&nbsp;'.repeat(q[0].length*2+1)+q[1]+'<br>'+q[2]}`
+            }
+            el.style.display='block'
+            document.getElementById('answer').focus();
+            el.querySelector('form').addEventListener('submit', function(e) {
+                e.preventDefault()
+                let answer=(el.querySelector("input[type='text']").value).trim()
+                if (answer) {
+                    setCookie('answer', answer)
+                    location.reload()
+                } else {
+                    alert('عليك أن تكتب')
+                }
+            })
+        }
+    }
+}
 
 window.addEventListener('load', function() {
     document.querySelectorAll("input[type='submit']").forEach(function(button) {
@@ -156,9 +174,13 @@ window.addEventListener('load', function() {
 
 const inp = document.getElementById('answer')
 inp.addEventListener('beforeinput', (e) => {
-if (e.inputType === 'insertText') {
-    e.preventDefault();
-    inp.value = e.data + inp.value;
-    inp.setSelectionRange(0, 0);
-  }
+    if (e.inputType === 'insertText') {
+        e.preventDefault();
+        inp.value = e.data + inp.value;
+        inp.setSelectionRange(0, 0);
+    } else if (e.inputType === 'deleteContentBackward') {
+        e.preventDefault()
+        inp.value = inp.value.slice(1)
+        inp.setSelectionRange(0, 0);
+    }
 });
